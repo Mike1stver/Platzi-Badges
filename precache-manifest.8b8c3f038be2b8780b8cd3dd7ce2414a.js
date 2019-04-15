1c4fb7462be9e15a79ec6bd3cb1b9cd0b75978fc
Miguel <miguel.fabian.bernal@gmail.com>
2019-04-15 17:55:15 -0500

    Updates

---
+++ precache-manifest.8b8c3f038be2b8780b8cd3dd7ce2414a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/Platzi-Badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "4b96c3dfa6b01eea4337",
+    "url": "/Platzi-Badges/static/css/main.a78dcb8f.chunk.css"
+  },
+  {
+    "revision": "ca79e1d267b0de3bbf82",
+    "url": "/Platzi-Badges/static/js/runtime~main.ca79e1d2.js"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/Platzi-Badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "7d81ba5d429947569ba5",
+    "url": "/Platzi-Badges/static/js/2.7d81ba5d.chunk.js"
+  },
+  {
+    "revision": "4b96c3dfa6b01eea4337",
+    "url": "/Platzi-Badges/static/js/main.4b96c3df.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/Platzi-Badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/Platzi-Badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/Platzi-Badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "7d81ba5d429947569ba5",
+    "url": "/Platzi-Badges/static/css/2.7550bb7d.chunk.css"
+  },
+  {
+    "revision": "c46dcf28760f89b482751f1d835d2124",
+    "url": "/Platzi-Badges/index.html"
+  }
+];
